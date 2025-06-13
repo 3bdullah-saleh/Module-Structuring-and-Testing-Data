484bd70717cefaa7086f728ab5a318d60341a615
3bdullah-saleh <abdullahedu0@gmail.com>
2025-06-13 22:46:32 +0100

    compare between two values

---
+++ prep/prep-sprint2/comparison.js
@@ -0,0 +1,8 @@
+let value1 = '100';
+let value2 = '23';
+console.log(value1 == value2);
+console.log(value1 > value2);
+
+value1 = Number(value1);
+value2 = Number(value2);
+console.log(value1 > value2);
