aa211fadda8813883a779a24371179dc21dcdcd3
3bdullah-saleh <abdullahedu0@gmail.com>
2025-06-13 21:21:55 +0100

    practice during preparation study

---
+++ prep/prep-sprint1/add2Numbers.py
@@ -0,0 +1,12 @@
+def main():
+    print("This program adds two numbers.")
+    num1 = input("Enter first number: ")
+    num1 = int(num1)
+    num2 = input("Enter second number: ")
+    num2 = int(num2)
+    total = num1 + num2 
+    print("The total is " + str(total) + ".")
+    num3 = 1.9 * 2
+    print(num3)
+
+main()  # 👈 This tells Python to actually run the function
---
+++ prep/prep-sprint1/example.js
@@ -0,0 +1,27 @@
+const yearOfBirth = 1990; //declaration
+let currentYear = 2025; //declaration
+currentYear++; //statement
+`I am ${currentYear - yearOfBirth} years old`; //statement 
+console.log("hello there!");
+
+const firstName = "Francesco";
+const age = 33;
+const nationality = "Italian";
+const result = Math.round(10.3);
+console.log("hello there!");
+
+
+// const volunteer = "Shadi";
+// // const volunteer = "Abdi";
+// // const volunteer = "Shadi";
+// volunteer = "Hinde";
+
+// console.log(Math.round(10.3);
+
+// For example, given the decimal number 0.5 we return the string "50%". 
+//Given the decimal number 0.231 we return the string "23.1%".
+// convertToPercentage(0.5); // should return "50%"
+// convertToPercentage(0.231); // should return "23.1%"
+const decimalNumber = 0.5;
+const percentage = `${decimalNumber * 100}%`
+console.log(`${percentage}`)
