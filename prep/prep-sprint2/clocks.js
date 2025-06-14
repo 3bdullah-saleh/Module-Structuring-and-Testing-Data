e74ba687737f8e9f1c7b3211d59d5cedc95d3b1e
3bdullah-saleh <abdullahedu0@gmail.com>
2025-06-15 00:42:27 +0100

    Update formatAs12HourClock to correctly handle PM times

--- prep/prep-sprint2/clocks.js
+++ prep/prep-sprint2/clocks.js
@@ -1,6 +1,9 @@
 function formatAs12HourClock(time) {
-  if (Number(time.slice(0, 2)) > 12) {
-    return `${Number(time.slice(0, 2)) - 12}:00 pm`;
+  let hours = Number(time.slice(0, 2));
+  if (hours > 12) {
+    hours = hours - 12;
+    keepTimeMinutes = time.slice(3,5)
+    return `${hours}:${keepTimeMinutes} pm`; 
   }
   return `${time} am`;
 }
@@ -20,9 +23,25 @@ console.assert(
   `current output: ${currentOutput2}, target output: ${targetOutput2}`
 );
 
-// const currentOutput3 = formatAs12HourClock("15:45");
-// const targetOutput3 = "03:45 pm";
-// console.assert(
-//     currentOutput3 === targetOutput3,
-//     `current output: ${currentOutput3}, target output: ${targetOutput3}`
-// );
+const currentOutput3 = formatAs12HourClock("15:45");
+const targetOutput3 = "03:45 pm";
+console.assert(
+    currentOutput3 === targetOutput3,
+    `current output: ${currentOutput3}, target output: ${targetOutput3}`
+);
+
+const currentOutput4 = formatAs12HourClock("01:01");
+const targetOutput4 = "01:01 am"
+console.assert(
+  currentOutput4 === targetOutput4,
+  `current output: ${currentOutput4}, target output: ${targetOutput4}`
+);
+
+const currentOutput5 = formatAs12HourClock("22:10");
+const targetOutput5 = "10:10 pm"
+console.assert(
+  currentOutput5 === targetOutput5,
+  `current output: ${currentOutput5}, target output: ${targetOutput5}`
+);
+
+
